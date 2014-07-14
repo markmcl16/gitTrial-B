bc08ac910feca52c84805627377e4a786620644b
Owner <Owner@131.202.107.31>
2014-07-14 13:57:19 -0300

    basic math functions

---
+++ types.py
@@ -0,0 +1,15 @@
+import math
+
+x = 10
+y = 3
+print x + y
+print x-y
+print x*y
+print x/y
+print float(x)/y
+radius = 2
+print 'area =', math.pi*radius*2
+print math.cos(math.pi)
+import numpy as np
+print 'area =', np.pi*radius*2
+print np.cos(np.pi)
