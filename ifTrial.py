9f37c3d6aef6b73fae26fb43057e943da8dc7f17
Owner <Owner@131.202.107.31>
2014-07-14 14:34:09 -0300

    if and else push

---
+++ ifTrial.py
@@ -0,0 +1,22 @@
+daveAge = 23
+jeffAge = 28
+
+if daveAge>jeffAge:
+    print 'dave is older than jeff'
+
+if jeffAge>daveAge:
+    print 'jeff is older than dave'
+
+if daveAge>jeffAge:
+    print 'dave is older than jeff'
+else:
+    print 'jeff is older than dave'
+
+if daveAge>jeffAge:
+    print 'dave is older than jeff'
+elif jeffAge>daveAge:
+    print 'jeff is older than dave'
+else:
+    print 'jeff and dave are same age'
+    
+        
